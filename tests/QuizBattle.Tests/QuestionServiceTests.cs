53a6dff1e3345bce6df117d0b6bafa5802d6da80
ulfnewton <ulf.bourelius@edu.newton.se>
2026-01-13 19:36:04 +0100

    Add Infrastructure project and refactor tests
    Added QuizBattle.Infrastructure to solution and test project references. Updated global.json to .NET 8.0.413. Refactored QuestionServiceTests to use async exception assertions and cleaned up usings.

--- tests/QuizBattle.Tests/QuestionServiceTests.cs
+++ tests/QuizBattle.Tests/QuestionServiceTests.cs
@@ -1,20 +1,23 @@
-﻿
-using System;
+﻿using QuizBattle.Application.Interfaces;
+using QuizBattle.Application.Services;
 using QuizBattle.Console;
+using QuizBattle.Infrastructure;
+using QuizBattle.Infrastructure.Repositories;
 using Xunit;
-
 namespace QuizBattle.Tests
 {
     public class QuestionServiceTests
     {
         [Fact]
-        public void GetRandomQuestions_CountGreaterOrEqualToTotal_Throws()
+        public async Task GetRandomQuestions_CountGreaterOrEqualToTotal_Throws()
         {
             var repo = new InMemoryQuestionRepository();
-            var service = new QuestionService(repo);
+            IQuestionService service = new QuestionService(repo); // vi specificerar att vi har en IQuestionService för att slippa kasta om senare
 
             // För närvarande finns 3 seedade frågor. Begär 4 => ska kasta.
-            Assert.Throws<ArgumentOutOfRangeException>(() => service.GetRandomQuestionsAsync(4));
+
+            // använd ThrowsAsync med await för att hantera det förväntade felet
+            await Assert.ThrowsAsync<ArgumentOutOfRangeException>(() => service.GetRandomQuestionsAsync(4));
         }
     }
 }
